878d53091e20b9fd785fff154a1e619a5b9c5c4e
Mehmet Emre Aktaş <94667868+emreakts@users.noreply.github.com>
2022-12-16 15:13:41 +0300

    Add files via upload

---
+++ guessthenumber.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine(" **** 23.Ders **** ");
+
+int tutulan = (new Random().Next(1,100));
+int hak = 5;
+System.Console.WriteLine(tutulan);
+while (hak>0)
+{
+    System.Console.WriteLine("Bir Sayi Giriniz : ");
+    int sayi = int.Parse(Console.ReadLine());
+    if (sayi == tutulan)
+    {
+        System.Console.WriteLine("Tahmininiz Doğru.Tebrikler !! ");
+        break;
+    }
+    else
+        {
+        System.Console.WriteLine(" Tekrar Deneyiniz ");
+        if (tutulan>sayi)
+            System.Console.WriteLine("Daha Büyük Sayi Tahmin Ediniz :");    
+        else
+             System.Console.WriteLine("Daha Düşük Sayi Tahmin Ediniz : ");
+        }
+        hak--;
+}
+    
