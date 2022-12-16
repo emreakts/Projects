344de1f220b64cf5dc45cce20734115a7df5485b
Mehmet Emre Aktaş <94667868+emreakts@users.noreply.github.com>
2022-12-16 14:34:48 +0300

    Add files via upload

---
+++ isasal.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine(" **** 22.Ders_uygulama **** ");
+
+bool isasal = true ;
+
+System.Console.Write(" Bir Sayi Giriniz : ");
+int sayi =int.Parse(System.Console.ReadLine());
+
+if (sayi==1)
+{ isasal=false;
+    System.Console.WriteLine(" Sayi Asal");
+}
+
+for (int i = 2; i < sayi; i++)
+{
+    if(sayi%i==0){
+        isasal=false;
+        System.Console.WriteLine("Sayi Asal Değildir");
+        break;
+    }
+}
+    if (isasal)
+    {
+        System.Console.WriteLine("Sayi Asaldır");
+    }
