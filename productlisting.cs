ba2f9070aaa47148a7238cb16c9a4c59c55dcc01
Mehmet Emre Aktaş <94667868+emreakts@users.noreply.github.com>
2022-12-16 15:12:36 +0300

    Add files via upload

---
+++ productlisting.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine(" **** 20.ders **** ");
+
+Console.Write("Adet Sayisi Giriniz :");
+ int adet = int.Parse(Console.ReadLine());
+
+string[] urunler = new string[adet];
+
+ int i = 0 ;
+ do
+ {
+    Console.Write($" {i+1}. Ürün Adı Giriniz : ");
+    urunler[i] = Console.ReadLine();
+    i++;
+ } while (adet!=i);
+
+Console.WriteLine(" Ürünler Listeleniyor ...");
+
+for (int a = 0; a < urunler.Length; a++)
+{
+Console.WriteLine($" {a+1}. Ürün Adı : {urunler[a]}" );
+}
