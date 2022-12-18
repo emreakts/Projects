e9a70788e78bd0f31749a2c54253465a0b07155d
Mehmet Emre Aktaş <94667868+emreakts@users.noreply.github.com>
2022-12-18 22:15:21 +0300

    Update guessthenumber.cs

--- guessthenumber.cs
+++ guessthenumber.cs
@@ -1,24 +1,39 @@
+do {
 int tutulan = (new Random().Next(1,100));
-int hak = 2;
+int hak = 5;
+int tur=0;
+double sp = 100/hak;
+
 System.Console.WriteLine(tutulan);
 while (hak>0)
 {
+    tur++;
+    System.Console.WriteLine($"{tur}. Deneme");
     System.Console.Write("Bir Sayi Giriniz : ");
     int sayi = int.Parse(Console.ReadLine());
     hak--;
     if (sayi == tutulan)
     {
-        System.Console.Write("Tahmininiz Doğru.Tebrikler !! ");
+        double puan = 100-(sp*(tur-1));
+        System.Console.WriteLine($"{tur}. Denemede Tahmininiz Doğru Tebrikler !! ");
+        System.Console.WriteLine($"Kazanilan Puan {puan}");
         break;
     }
     else
         {
         if (hak==0)
-            break;
+        break;
         if (tutulan>sayi)
             System.Console.WriteLine("Daha Büyük Sayi Tahmin Ediniz ");    
         else
              System.Console.WriteLine("Daha Düşük Sayi Tahmin Ediniz ");
-        }
-}
-    System.Console.Write("Hakkiniz Bitmistir.");
+        }    
+}       System.Console.WriteLine("Hakkiniz Bitmistir.");
+        System.Console.Write(" Devam Etmek istiyor musunuz ? (e/h) : ");
+
+        string result = Console.ReadLine();
+
+                if (result=="h" || result =="hayır")
+                    break;
+  }  while (true) ;
+    
